a2ee6c6ad9d9f0d762c79850dc47b427129260ab
Jussi Sirén <jussi.siren@sleek.works>
2025-05-02 09:16:54 +0300

    OPHJOD-1563: add ArtikkelinKatselu

---
+++ src/test/java/fi/okm/jod/ohjaaja/service/ArtikkelinKatseluServiceTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2025 The Finnish Ministry of Education and Culture, The Finnish
+ * The Ministry of Economic Affairs and Employment, The Finnish National Agency of
+ * Education (Opetushallitus) and The Finnish Development and Administration centre
+ * for ELY Centres and TE Offices (KEHA).
+ *
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
+package fi.okm.jod.ohjaaja.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import fi.okm.jod.ohjaaja.repository.ArtikkelinKatseluRepository;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
+@Import({ArtikkelinKatseluService.class})
+class ArtikkelinKatseluServiceTest extends AbstractServiceTest {
+
+  @Autowired private ArtikkelinKatseluService service;
+  @Autowired private ArtikkelinKatseluRepository repository;
+
+  @Test
+  void shouldAddArtikkelinKatseluForAnoonymiId() {
+    UUID ohjaajaId = null;
+    var anonyymiId = "anonyymi123";
+    var artikkeliId = 1L;
+    var id = service.add(ohjaajaId, anonyymiId, artikkeliId);
+
+    assertNotNull(id);
+    var katselu = repository.findById(id).orElseThrow();
+    assertEquals(ohjaajaId, katselu.getOhjaajaId());
+    assertEquals(anonyymiId, katselu.getAnonyymiId());
+    assertEquals(artikkeliId, katselu.getArtikkeliId());
+    assertNotNull(katselu.getLuotu());
+  }
+
+  @Test
+  void shouldAddArtikkelinKatseluForOhjaajaId() {
+    UUID ohjaajaId = UUID.randomUUID();
+    String anonyymiId = null;
+    var artikkeliId = 1L;
+    var id = service.add(ohjaajaId, anonyymiId, artikkeliId);
+
+    assertNotNull(id);
+    var katselu = repository.findById(id).orElseThrow();
+    assertEquals(ohjaajaId, katselu.getOhjaajaId());
+    assertEquals(anonyymiId, katselu.getAnonyymiId());
+    assertEquals(artikkeliId, katselu.getArtikkeliId());
+    assertNotNull(katselu.getLuotu());
+  }
+
+  @Test
+  void shouldNotAddArtikkelinKatseluForBothOhjaajaIdAndAnonyymiId() {
+    UUID ohjaajaId = UUID.randomUUID();
+    String anonyymiId = "anonyymi123";
+    var artikkeliId = 1L;
+
+    assertThrows(
+        ServiceValidationException.class, () -> service.add(ohjaajaId, anonyymiId, artikkeliId));
+  }
+
+  @Test
+  void shouldNotAddArtikkelinKatseluForNeitherOhjaajaIdNorAnonyymiId() {
+    UUID ohjaajaId = null;
+    String anonyymiId = null;
+    var artikkeliId = 1L;
+
+    assertThrows(
+        ServiceValidationException.class, () -> service.add(ohjaajaId, anonyymiId, artikkeliId));
+  }
+}
