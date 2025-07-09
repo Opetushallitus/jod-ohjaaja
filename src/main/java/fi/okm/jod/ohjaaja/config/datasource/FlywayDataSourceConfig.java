843c129eb3512ce1187e2ef0439003837cae9024
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2025-07-09 14:44:53 +0300

    OPHJOD-1771: Update datasource configuration

--- src/main/java/fi/okm/jod/ohjaaja/config/datasource/FlywayDataSourceConfig.java
+++ src/main/java/fi/okm/jod/ohjaaja/config/datasource/FlywayDataSourceConfig.java
@@ -12,12 +12,12 @@ package fi.okm.jod.ohjaaja.config.datasource;
 import java.util.Objects;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
@@ -27,8 +27,8 @@ public class FlywayDataSourceConfig {
 
   /** Configures Flyway to use IAM authentication with RDS. */
   @Bean
-  @Profile("cloud")
-  public FlywayConfigurationCustomizer flywayConfigurationCustomizer(
+  @ConditionalOnBean(RdsIamAuthTokenProvider.class)
+  FlywayConfigurationCustomizer flywayConfigurationCustomizer(
       RdsIamAuthTokenProvider authTokenProvider) {
     return configuration -> {
       var flywayDataSource =
