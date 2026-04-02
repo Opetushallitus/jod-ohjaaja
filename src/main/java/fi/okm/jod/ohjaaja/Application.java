9179840e518cd9884c8de3226a91cc66260d4ca9
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2026-04-02 11:12:55 +0300

    OPHJOD-3292: Add user count metric

--- src/main/java/fi/okm/jod/ohjaaja/Application.java
+++ src/main/java/fi/okm/jod/ohjaaja/Application.java
@@ -12,10 +12,12 @@ package fi.okm.jod.ohjaaja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /** Application entrypoint. */
 @SpringBootApplication
 @ConfigurationPropertiesScan("fi.okm.jod.ohjaaja")
+@EnableScheduling
 public class Application {
 
   public static void main(String[] args) {
